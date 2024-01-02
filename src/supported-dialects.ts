08f67e9596d5a9c4651450d5ce0c043d0246020a
igalklebanov <igalklebanov@gmail.com>
2024-01-02 19:36:23 +0200

    supported-dialects.

---
+++ src/supported-dialects.ts
@@ -0,0 +1,21 @@
+import type {DataSourceOptions} from 'typeorm'
+
+export const SUPPORTED_DIALECTS = [
+  'better-sqlite3',
+  'mssql',
+  'mysql',
+  'postgres',
+  'sqlite',
+] as const satisfies DataSourceOptions['type'][]
+
+export type SupportedDialect = (typeof SUPPORTED_DIALECTS)[number]
+
+function isDialectSupported(dialect: string): dialect is SupportedDialect {
+  return SUPPORTED_DIALECTS.includes(dialect as any)
+}
+
+export function assertSupportedDialect(dialect: string) {
+  if (!isDialectSupported(dialect)) {
+    throw new Error(`Unsupported dialect: ${dialect}!`)
+  }
+}
